a88b396b63ded6b8ea6cc9e00b3d84455a7cb569
Freeman12321 <freeman.abrahams@s.elim.school.nz>
2020-02-27 14:34:21 +1300

    Asset-store-incorporation

---
+++ Survivor/Assets/loadLevel1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class loadLevel1 : MonoBehaviour {
+
+    public void loadNextLevel() {
+        Debug.Log("Next Level");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
